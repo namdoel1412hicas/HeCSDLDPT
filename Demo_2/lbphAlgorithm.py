bcb144c421a4caa9325b88d96b5065a7623d5538
Do Dinh Nam <namdoel1412@gmail.com>
2021-04-30 22:53:32 +0700

    hello

--- Demo_2/lbphAlgorithm.py
+++ Demo_2/lbphAlgorithm.py
@@ -2,17 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt      # importing the matplotlib and cv2 for plottig and reading the image file
 import cv2
-from google.colab import files       # uploading the file to google colabs from PC
+import pandas as pd
+# from google.colab import files       # uploading the file to google colabs from PC
 
-uploaded = files.upload()
+# uploaded = files.upload()
 
-for fn in uploaded.keys():
-  print('User uploaded file "{name}" with length {length} bytes'.format(
-      name=fn, length=len(uploaded[fn])))
-from google.colab.patches import cv2_imshow        # converting the image to gray scale image    
-img= cv2.imread("ak fsw.jpg")
+# for fn in uploaded.keys():
+#   print('User uploaded file "{name}" with length {length} bytes'.format(
+#       name=fn, length=len(uploaded[fn])))
+# from google.colab.patches import cv2_imshow        # converting the image to gray scale image    
+img= cv2.imread("data/test/harry_test_2.jpg")
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2_imshow(gray_img)
+cv2.imshow('Gray Scale', gray_img)
 def Binarypattern(im):                               # creating function to get local binary pattern
     img= np.zeros_like(im)
     n=3                                              # taking kernel of size 3*3
@@ -31,8 +32,13 @@ def Binarypattern(im):                               # creating function to get 
             img[i+1,j+1] = num
     return(img)
 
-imgLBP=Binarypattern(gray_img)             # calling the LBP function using gray image
+imgLBP=Binarypattern(gray_img)
+cv2.imshow('Gray Scale 2', imgLBP)
+             # calling the LBP function using gray image
+# chuyển ảnh từ ma trận -> vector
 vectorLBP = imgLBP.flatten()               # for histogram using the vector form of image pixels
+#pd.DataFrame(vectorLBP).to_csv("data/csvfiles/harry_.csv")
+#print(vectorLBP)
 fig=plt.figure(figsize=(20,8))             #subplotting the gray, LBP and histogram 
 ax  = fig.add_subplot(1,3,1)
 ax.imshow(gray_img)
@@ -41,7 +47,8 @@ ax  = fig.add_subplot(1,3,2)
 ax.imshow(imgLBP,cmap="gray")
 ax.set_title("LBP converted image")
 ax  = fig.add_subplot(1,3,3)
-freq,lbp, _ = ax.hist(vectorLBP,bins=2**8)       
+print(ax.hist(vectorLBP,bins=2**8)) # in ra giá trị của histogram
+freq,lbp, _ = ax.hist(vectorLBP,bins=2**8)
 ax.set_ylim(0,40000)
 lbp = lbp[:-1]
 ## print the LBP values when frequencies are high
